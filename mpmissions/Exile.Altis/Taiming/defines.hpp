1c9f719ea01381105a7d6b5b2172f190650d3ea7
Tom Witkowski <dev.gummibeer@gmail.com>
2016-06-24 11:09:05 +0200

    fix sounds

--- mpmissions/Exile.Altis/Taiming/defines.hpp
+++ mpmissions/Exile.Altis/Taiming/defines.hpp
@@ -4,31 +4,31 @@ class CfgSounds
     class dog_growl
     {
         name = "dog_growl";
-        sound[] = {"sounds\dog_growl.ogg",1.0,1.0};
+        sound[] = {"Taiming\sounds\dog_growl.ogg",1.0,1.0};
         titles[] = {};
     };
     class dog_one
     {
         name = "dog_one";
-        sound[] = {"sounds\dog_one.ogg",1.0,1.0};
+        sound[] = {"Taiming\sounds\dog_one.ogg",1.0,1.0};
         titles[] = {};
     };
     class dog_ruff
     {
         name = "dog_ruff";
-        sound[] = {"sounds\dog_ruff.ogg",1.0,1.0};
+        sound[] = {"Taiming\sounds\dog_ruff.ogg",1.0,1.0};
         titles[] = {};
     };
     class dog_whine
     {
         name = "dog_whine";
-        sound[] = {"sounds\dog_whine.ogg",1.0,1.0};
+        sound[] = {"Taiming\sounds\dog_whine.ogg",1.0,1.0};
         titles[] = {};
     };
     class whistle
     {
         name = "whistle";
-        sound[] = {"sounds\whistle.ogg",1.0,1.0};
+        sound[] = {"Taiming\sounds\whistle.ogg",1.0,1.0};
         titles[] = {};
     };
 };
