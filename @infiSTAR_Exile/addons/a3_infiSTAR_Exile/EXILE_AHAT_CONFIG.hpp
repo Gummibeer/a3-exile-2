8fe0ccd56339e45c826d52afaa39b3b1fc36fa07
Tom Witkowski <dev.gummibeer@gmail.com>
2016-06-28 22:13:35 +0200

    #47 adjust viewdistance

--- @infiSTAR_Exile/addons/a3_infiSTAR_Exile/EXILE_AHAT_CONFIG.hpp
+++ @infiSTAR_Exile/addons/a3_infiSTAR_Exile/EXILE_AHAT_CONFIG.hpp
@@ -285,8 +285,8 @@ VCT = 300;	/* if "DayNightVote = true;" - Time (in seconds) to wait until nex vo
 
 /* The following 4 options can be disabled by putting the value to -1. For example "TGV = -1;" */
 /* Terrain Grid Value   */ TGV = 40;	/* 50, 25, 12.5  */	/* if set to 50 grass will be very low for better client FPS.. default is 25 ~35 is good performance and grass :) */
-/* ViewDistance Value   */ VDV = 1000;
-/* ObjectViewDistance   */ VOV = 1000;
+/* ViewDistance Value   */ VDV = 2000;
+/* ObjectViewDistance   */ VOV = 2000;
 /* ShadowViewDistance   */ SVD = 50;
 
 
